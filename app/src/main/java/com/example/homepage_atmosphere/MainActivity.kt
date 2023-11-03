ecf44ffaa52f243d0f8f6c923b5a7d0c35b0b8a5
SmartGuy <kadiayieraste@mail.ru>
2023-11-03 12:58:36 +0300

    TopBar first step

--- app/src/main/java/com/example/homepage_atmosphere/MainActivity.kt
+++ app/src/main/java/com/example/homepage_atmosphere/MainActivity.kt
@@ -8,86 +8,81 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.homepage_atmosphere.ui.theme.HomePage_AtmosphereTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomePage_AtmosphereTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    HomePage("Android")
-                }
-            }
+            ScaffoldCompose()
         }
     }
 }
-
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomePage(s:String) {
+fun ScaffoldCompose(){
+    Scaffold(
+        topBar = { TopAppBarCompose()}
+    ){ contentPadding ->
 
-        Row (modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 10.dp),
-            verticalAlignment = Alignment.Top,
-            horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-            Card(
-                elevation = CardDefaults.cardElevation(4.dp),
-                shape = RectangleShape,
-                modifier = Modifier.padding(start = 10.dp)) {
-                    Icon(painter = painterResource(id = R.drawable.icon_menu),
-                contentDescription = "icon_menu",
-                modifier = Modifier
-                    .size(20.dp)
-                    .padding(start = 0.dp))
-            }
+        Row(modifier = Modifier.padding(contentPadding)) {
 
-            Text(text= "Alleluia",
-                modifier = Modifier.)
+        }
 
-            Row (modifier = Modifier
-                .fillMaxSize(),
-                verticalAlignment = Alignment.Top,
-                horizontalArrangement = Arrangement.End
-            ){
-                Icon(painter = painterResource(id = R.drawable.icon_qr_code_scanner),
-                    contentDescription = "icon_menu",
-                    modifier = Modifier.size(20.dp))
-
-                Icon(painter = painterResource(id = R.drawable.icon_options),
-                    contentDescription = "icon_menu")
-            }
-
-
-            }
     }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarCompose(){
+    TopAppBar(
+        title = { Text(text = "Alleluia",
+            fontSize = 20.sp, color = Color.Black,
+            fontWeight = FontWeight.Bold)},
+        navigationIcon = {
+                     IconButton(onClick = {
 
-
-data class Person(val person: Person)
+                     }) {
+                         Icon(Icons.Default.Menu,
+                             contentDescription = "icon_menu")
+                     }
+        },
+        actions = {}
+    )
+}
