56ad5b74602a6c9dba5d5bea769e31182bf2db27
SmartGuy <kadiayieraste@mail.ru>
2023-11-04 14:49:33 +0300

    bad version

--- app/src/main/java/com/example/homepage_atmosphere/MainActivity.kt
+++ app/src/main/java/com/example/homepage_atmosphere/MainActivity.kt
@@ -4,7 +4,6 @@ import MainViewModel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults.cardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -22,22 +20,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.homepage_atmosphere.ui.theme.HomePage_AtmosphereTheme
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.stateIn
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,15 +42,13 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ScaffoldCompose(){
-    Scaffold(
-        topBar = { TopAppBarCompose()},
-        bottomBar = { BottomAppBarCompose()}
-    ){ contentPadding ->
+fun ScaffoldCompose() {
+    Scaffold(topBar = { TopAppBarCompose() },
+        bottomBar = { BottomAppBarCompose() }) { contentPadding ->
 
         Card(elevation = cardElevation(defaultElevation = 10.dp)) {
             val viewModel = viewModel<MainViewModel>()
-            val searchText by viewModel.searchText.collectAsState()
+            val searchText = "by viewModel.searchText.collectAsState()"
             val songs by viewModel.songs.collectAsState()
             val isSearching by viewModel.isSearching.collectAsState()
             Column(
@@ -74,12 +57,13 @@ fun ScaffoldCompose(){
                     .fillMaxSize()
                     .padding(3.dp)
             ) {
-                TextField(
-                    value = searchText,
-                    OnvalueChange = viewModel::onSearchTextChange,
-                    modifier = Modifier.fillMaxWidth(),
-                    placeholder = {Text(text = "Search songs...")}
-                )
+                TextField(value = searchText, onValueChange = viewModel::onSearchTextChange)
+//                TextField(
+//                    value = searchText,
+//                    OnvalueChange = viewModel::onSearchTextChange,
+////                    modifier = Modifier.fillMaxWidth(),
+////                    placeholder = Text(text = "Search songs...")
+//                )
                 Spacer(modifier = Modifier.height(2.dp))
                 LazyColumn(
                     modifier = Modifier
@@ -87,7 +71,7 @@ fun ScaffoldCompose(){
                         .fillMaxWidth()
                         .weight(1f)
                 ) {
-                    items(songs) {song ->
+                    items(songs) { song ->
                         Text(
                             text = "${song.id} ${song.song}",
                             modifier = Modifier
---
+++ app/src/main/java/com/example/homepage_atmosphere/Search_Bar.kt
@@ -0,0 +1,4 @@
+package com.example.homepage_atmosphere
+
+class Search_Bar {
+}
